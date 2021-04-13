7acd8a1f35753d481d9f3faeacc98015d7e28ea1
Diokar <82386010+Diokar@users.noreply.github.com>
2021-04-13 14:39:43 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,13 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+my_list = [0, 10, 2, 1, -12]
+summ = 0
+summ1 = 0
+i = 0
+for summ in my_list:
+    if summ > 0:
+        summ1 = summ1 + summ
+
+
+print(summ1)
