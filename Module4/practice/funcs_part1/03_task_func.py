e1737a0d846201eb02888ef96d2dff5d668a8710
Diokar <82386010+Diokar@users.noreply.github.com>
2021-04-14 12:15:35 +0300

    Update 03_task_func.py

--- Module4/practice/funcs_part1/03_task_func.py
+++ Module4/practice/funcs_part1/03_task_func.py
@@ -2,7 +2,9 @@
 # Напишите функцию нахождения расстояния между двумя точками.
 
 def distance(x1, y1, x2, y2):
-    # TODO: your code here
+    s  = round((((x2**2 - x1**2)+(y2**2 - y1**2))**0.5),2)
+    s = round(s,3)
+    return s
     pass
 
 
